6a48f89efdd60e3a68b5cc7eb1b1c8b37de68225
Abdulmajeed Alasmari <ialasmariabdulmajeed@gmail.com>
2022-03-08 10:14:41 +0300

    Add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
       address: 'localhost',
       port: 1025
     }
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
